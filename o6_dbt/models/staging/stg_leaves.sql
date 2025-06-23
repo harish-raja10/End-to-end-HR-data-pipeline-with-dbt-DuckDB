27a7b0da56e4aeea1eb04df7612e56896b00afa0
harish-raja10 <53789536+harish-raja10@users.noreply.github.com>
2025-06-23 22:48:28 +0530

    stg_leaves.sql

--- o6_dbt/models/staging/stg_leaves.sql
+++ o6_dbt/models/staging/stg_leaves.sql
@@ -1,9 +1,8 @@
 SELECT
   CAST(id AS INT) AS id,
-  TRIM(first_name) AS first_name,
-  TRIM(last_name) AS last_name,
-  CAST(join_date AS DATE) AS join_date,
-  TRIM(dept) AS dept,
+  CAST(employee_id AS INT) AS employee_id,
+  CAST(date AS DATE) AS date,
+  TRIM(type) AS type,
   CURRENT_TIMESTAMP AS loaded_at
-FROM read_parquet('../lake/bronze/employees/*/*.parquet')
+FROM read_parquet('../lake/bronze/leaves/*/*.parquet')
 WHERE id IS NOT NULL
